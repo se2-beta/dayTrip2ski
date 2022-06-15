f1f3ff2de44c3afc6ed37f238a406848173cd668
Andreas <sa0543@mci4me.at>
2022-06-15 10:36:46 +0200

    Update column names

--- src/main/java/com/application/views/masterdetail/MasterDetailView.java
+++ src/main/java/com/application/views/masterdetail/MasterDetailView.java
@@ -79,9 +79,29 @@ public class MasterDetailView extends VerticalLayout {
     }
 
     private void configureGrid() {
+
         grid.addClassNames("contact-grid");
         grid.setSizeFull();
-        grid.setColumns("name", "region", "operator", "address", "zip", "city", "heightMin", "heightMax", "totalLength", "ropeways", "posLon", "posLat", "dateSeasonStart", "dateSeasonEnd", "timeServiceStart", "timeServiceEnd", "URLTicketpage", "URLImageFront", "URLImageSlope");
+        grid.setColumns();
+        grid.addColumn(SkiResort::getName).setHeader("Name");
+        grid.addColumn(SkiResort::getRegion).setHeader("Region");
+        grid.addColumn(SkiResort::getOperator).setHeader("Betreiber");
+        grid.addColumn(SkiResort::getAddress).setHeader("Addresse");
+        grid.addColumn(SkiResort::getZip).setHeader("Postleitzahl");
+        grid.addColumn(SkiResort::getCity).setHeader("Stadt");
+        grid.addColumn(SkiResort::getHeightMin).setHeader("Schneehöhe min");
+        grid.addColumn(SkiResort::getHeightMax).setHeader("Schneehöhe max");
+        grid.addColumn(SkiResort::getTotalLength).setHeader("Streckenlänge");
+        grid.addColumn(SkiResort::getRopeways).setHeader("Pistenanzahl");
+        grid.addColumn(SkiResort::getPosLon).setHeader("Breitengrad");
+        grid.addColumn(SkiResort::getPosLat).setHeader("Längengrad");
+        grid.addColumn(SkiResort::getDateSeasonStart).setHeader("Start-Saison");
+        grid.addColumn(SkiResort::getDateSeasonEnd).setHeader("Ende-Saison");
+        grid.addColumn(SkiResort::getTimeServiceStart).setHeader("Öffnungszeiten");
+        grid.addColumn(SkiResort::getTimeServiceEnd).setHeader("Schließzeiten");
+        grid.addColumn(SkiResort::getURLImageSlope).setHeader("Pistenbild");
+        grid.addColumn(SkiResort::getURLImageFront).setHeader("Titelbild");
+        grid.addColumn(SkiResort::getURLTicketpage).setHeader("Ticketseite");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
         grid.asSingleSelect().addValueChangeListener(e -> editContact(e.getValue()));
     }
