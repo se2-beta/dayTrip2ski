02e5f98bbf0af519f12d79484164e9e481457822
LukasG <gl1935@mci4me.at>
2022-06-16 21:06:43 +0200

    changed LoginView to extend Main, added background image, changed UI to German

--- src/main/java/com/application/views/login/LoginView.java
+++ src/main/java/com/application/views/login/LoginView.java
@@ -1,25 +1,51 @@
 package com.application.views.login;
 
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
-import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Login")
 @Route(value = "login")
-public class LoginView extends LoginOverlay {
+public class LoginView extends Main {
+
     public LoginView() {
-        setAction("login");
+
+        setSizeFull();
+        getElement().getStyle().set("background-image", "url('https://www.tyrol.com/portal/image.php?idPart=vwahqircnxy-')").set("background-size", "cover");
 
         LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("daytrip2Ski");
-        i18n.getHeader().setDescription("Login using user/user or admin/admin");
-        i18n.setAdditionalInformation(null);
-        setI18n(i18n);
+        LoginI18n.Form form = i18n.getForm();
+        form.setTitle("daytrip2Ski");
+        form.setUsername("Benutzername");
+        form.setPassword("Passwort");
+        form.setSubmit("Anmelden");
+        i18n.setForm(form);
+        LoginI18n.ErrorMessage error = new LoginI18n.ErrorMessage();
+        error.setMessage("Bitte geben sie ihren Benutzernamen und ihr Passwort ein!");
+        i18n.setErrorMessage(error);
+        i18n.setAdditionalInformation("© Team Beta - Annina Ecker, Andreas Schöffel, David Zollitsch, Markus Russold, Julian Hirschberger, Lukas Gerber");
 
-        setForgotPasswordButtonVisible(false);
-        setOpened(true);
+        LoginForm lf = new LoginForm();
+        lf.setAction("login");
+        lf.setError(true);
+        lf.setForgotPasswordButtonVisible(false);
+        lf.setI18n(i18n);
+
+        FlexLayout flexLayout = new FlexLayout(lf);
+        flexLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        flexLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        flexLayout.setHeightFull();
+
+        add(
+                flexLayout
+
+        );
+
     }
 
 }
+
