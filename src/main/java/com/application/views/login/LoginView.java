1765f5d2f230ee8cf617065e851ad879e8abc945
LukasG <gl1935@mci4me.at>
2022-06-16 21:42:51 +0200

    changes to Error handling, added logo

--- src/main/java/com/application/views/login/LoginView.java
+++ src/main/java/com/application/views/login/LoginView.java
@@ -1,51 +1,55 @@
 package com.application.views.login;
 
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 
 @PageTitle("Login")
 @Route(value = "login")
 public class LoginView extends Main {
+
+    LoginForm loginForm = new LoginForm();
 
     public LoginView() {
 
         setSizeFull();
         getElement().getStyle().set("background-image", "url('https://www.tyrol.com/portal/image.php?idPart=vwahqircnxy-')").set("background-size", "cover");
 
+        Image titleLogo = new Image("images/daytrip2Ski_logo_transp.png", "logo");
+        titleLogo.setWidth("250px");
+
         LoginI18n i18n = LoginI18n.createDefault();
         LoginI18n.Form form = i18n.getForm();
-        form.setTitle("daytrip2Ski");
         form.setUsername("Benutzername");
         form.setPassword("Passwort");
         form.setSubmit("Anmelden");
         i18n.setForm(form);
-        LoginI18n.ErrorMessage error = new LoginI18n.ErrorMessage();
-        error.setMessage("Bitte geben sie ihren Benutzernamen und ihr Passwort ein!");
-        i18n.setErrorMessage(error);
         i18n.setAdditionalInformation("© Team Beta - Annina Ecker, Andreas Schöffel, David Zollitsch, Markus Russold, Julian Hirschberger, Lukas Gerber");
 
-        LoginForm lf = new LoginForm();
-        lf.setAction("login");
-        lf.setError(true);
-        lf.setForgotPasswordButtonVisible(false);
-        lf.setI18n(i18n);
 
-        FlexLayout flexLayout = new FlexLayout(lf);
-        flexLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        flexLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        flexLayout.setHeightFull();
+        loginForm.setAction("login");
+        loginForm.setForgotPasswordButtonVisible(false);
+        loginForm.setI18n(i18n);
+
+        VerticalLayout layout = new VerticalLayout(titleLogo, loginForm);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setHeightFull();
 
         add(
-                flexLayout
+                layout
 
         );
 
     }
-
 }
 
