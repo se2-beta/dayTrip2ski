9e6720dcbe5d76d48395d91b63b4942dd3455d97
AndreasSchoeffel <95340508+AndreasSchoeffel@users.noreply.github.com>
2022-06-16 14:20:46 +0200

    Master detail frontend (#7)
    * Admin Role + first list(incomplete)
    
    * add Form plus Buttons
    
    * Database access
    
    * finish list
    
    * access database
    
    * selection
    
    * function cancel
    
    * function save
    
    * Function delete
    
    * complet form
    
    * delete filter
    
    * add Notification
    
    * update_layout
    
    * update
    
    * extend list and form
    
    * clean code
    
    * clean code (names)
    
    * Update column names
    
    * update input admin

--- src/main/java/com/application/views/masterdetail/MasterDetailView.java
+++ src/main/java/com/application/views/masterdetail/MasterDetailView.java
@@ -1,247 +1,136 @@
 package com.application.views.masterdetail;
 
-import com.application.data.entity.SampleBook;
-import com.application.data.service.SampleBookService;
+import com.application.data.entity.SkiResort;
+import com.application.data.service.SkiResortService;
 import com.application.views.MainLayout;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.splitlayout.SplitLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
-import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.data.converter.StringToIntegerConverter;
-import com.vaadin.flow.data.renderer.LitRenderer;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
-import elemental.json.Json;
-import java.io.ByteArrayOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Optional;
-import java.util.UUID;
 import javax.annotation.security.RolesAllowed;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.web.util.UriUtils;
 
-@PageTitle("Master-Detail")
+
+@PageTitle("Admin-Bereich")
 @Route(value = "master-detail/:sampleBookID?/:action?(edit)", layout = MainLayout.class)
 @RolesAllowed("ADMIN")
-public class MasterDetailView extends Div implements BeforeEnterObserver {
+public class MasterDetailView extends VerticalLayout {
 
-    private final String SAMPLEBOOK_ID = "sampleBookID";
-    private final String SAMPLEBOOK_EDIT_ROUTE_TEMPLATE = "master-detail/%s/edit";
+    Grid<SkiResort> grid = new Grid<>(SkiResort.class);
+    SkiResortForm form;
+    private SkiResortService service;
 
-    private Grid<SampleBook> grid = new Grid<>(SampleBook.class, false);
-
-    private Upload image;
-    private Image imagePreview;
-    private TextField name;
-    private TextField author;
-    private DatePicker publicationDate;
-    private TextField pages;
-    private TextField isbn;
-
-    private Button cancel = new Button("Cancel");
-    private Button save = new Button("Save");
-
-    private BeanValidationBinder<SampleBook> binder;
-
-    private SampleBook sampleBook;
-
-    private final SampleBookService sampleBookService;
-
-    @Autowired
-    public MasterDetailView(SampleBookService sampleBookService) {
-        this.sampleBookService = sampleBookService;
-        addClassNames("master-detail-view");
-
-        // Create UI
-        SplitLayout splitLayout = new SplitLayout();
-
-        createGridLayout(splitLayout);
-        createEditorLayout(splitLayout);
-
-        add(splitLayout);
-
-        // Configure Grid
-        LitRenderer<SampleBook> imageRenderer = LitRenderer
-                .<SampleBook>of("<img style='height: 64px' src=${item.image} />")
-                .withProperty("image", SampleBook::getImage);
-        grid.addColumn(imageRenderer).setHeader("Image").setWidth("68px").setFlexGrow(0);
-
-        grid.addColumn("name").setAutoWidth(true);
-        grid.addColumn("author").setAutoWidth(true);
-        grid.addColumn("publicationDate").setAutoWidth(true);
-        grid.addColumn("pages").setAutoWidth(true);
-        grid.addColumn("isbn").setAutoWidth(true);
-        grid.setItems(query -> sampleBookService.list(
-                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
-                .stream());
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-
-        // when a row is selected or deselected, populate form
-        grid.asSingleSelect().addValueChangeListener(event -> {
-            if (event.getValue() != null) {
-                UI.getCurrent().navigate(String.format(SAMPLEBOOK_EDIT_ROUTE_TEMPLATE, event.getValue().getId()));
-            } else {
-                clearForm();
-                UI.getCurrent().navigate(MasterDetailView.class);
-            }
-        });
-
-        // Configure Form
-        binder = new BeanValidationBinder<>(SampleBook.class);
-
-        // Bind fields. This is where you'd define e.g. validation rules
-        binder.forField(pages).withConverter(new StringToIntegerConverter("Only numbers are allowed")).bind("pages");
-
-        binder.bindInstanceFields(this);
-
-        attachImageUpload(image, imagePreview);
-
-        cancel.addClickListener(e -> {
-            clearForm();
-            refreshGrid();
-        });
-
-        save.addClickListener(e -> {
-            try {
-                if (this.sampleBook == null) {
-                    this.sampleBook = new SampleBook();
-                }
-                binder.writeBean(this.sampleBook);
-                this.sampleBook.setImage(imagePreview.getSrc());
-
-                sampleBookService.update(this.sampleBook);
-                clearForm();
-                refreshGrid();
-                Notification.show("SampleBook details stored.");
-                UI.getCurrent().navigate(MasterDetailView.class);
-            } catch (ValidationException validationException) {
-                Notification.show("An exception happened while trying to store the sampleBook details.");
-            }
-        });
+    public MasterDetailView(SkiResortService service) {
+        this.service = service;
+        addClassNames("list-view");
+        setSizeFull();
+        configureGrid();
+        configureForm();
+        add(getToolbar(), getContent());
+        updateList();
+        closeEditor();
 
     }
 
-    @Override
-    public void beforeEnter(BeforeEnterEvent event) {
-        // Removed by Markus as this code is not needed, but would cause an exception due to a type conflict with the ID.
-        // Shall be removed completely eventually.
+    private void closeEditor(){
+        form.setSkiResort(null);
+        form.setVisible(false);
+        removeClassName("editing");
 
-//        Optional<UUID> sampleBookId = event.getRouteParameters().get(SAMPLEBOOK_ID).map(UUID::fromString);
-//        if (sampleBookId.isPresent()) {
-//            Optional<SampleBook> sampleBookFromBackend = sampleBookService.get(sampleBookId.get());
-//            if (sampleBookFromBackend.isPresent()) {
-//                populateForm(sampleBookFromBackend.get());
-//            } else {
-//                Notification.show(String.format("The requested sampleBook was not found, ID = %s", sampleBookId.get()),
-//                        3000, Notification.Position.BOTTOM_START);
-//                // when a row is selected but the data is no longer available,
-//                // refresh grid
-//                refreshGrid();
-//                event.forwardTo(MasterDetailView.class);
-//            }
-//        }
     }
 
-    private void createEditorLayout(SplitLayout splitLayout) {
-        Div editorLayoutDiv = new Div();
-        editorLayoutDiv.setClassName("editor-layout");
-
-        Div editorDiv = new Div();
-        editorDiv.setClassName("editor");
-        editorLayoutDiv.add(editorDiv);
-
-        FormLayout formLayout = new FormLayout();
-        Label imageLabel = new Label("Image");
-        imagePreview = new Image();
-        imagePreview.setWidth("100%");
-        image = new Upload();
-        image.getStyle().set("box-sizing", "border-box");
-        image.getElement().appendChild(imagePreview.getElement());
-        name = new TextField("Name");
-        author = new TextField("Author");
-        publicationDate = new DatePicker("Publication Date");
-        pages = new TextField("Pages");
-        isbn = new TextField("Isbn");
-        Component[] fields = new Component[]{imageLabel, image, name, author, publicationDate, pages, isbn};
-
-        formLayout.add(fields);
-        editorDiv.add(formLayout);
-        createButtonLayout(editorLayoutDiv);
-
-        splitLayout.addToSecondary(editorLayoutDiv);
+    private void updateList(){
+        grid.setItems(service.getAllSkiResort());
     }
 
-    private void createButtonLayout(Div editorLayoutDiv) {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.setClassName("button-layout");
-        cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(save, cancel);
-        editorLayoutDiv.add(buttonLayout);
+    private Component getContent(){
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, form);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
     }
 
-    private void createGridLayout(SplitLayout splitLayout) {
-        Div wrapper = new Div();
-        wrapper.setClassName("grid-wrapper");
-        splitLayout.addToPrimary(wrapper);
-        wrapper.add(grid);
+    public void configureForm(){
+        form = new SkiResortForm();
+        form.setWidth("30em");
+
+        form.addListener(SkiResortForm.SaveEvent.class, this::saveSkiresort);
+        form.addListener(SkiResortForm.DeleteEvent.class, this::deleteSkiresort);
+        form.addListener(SkiResortForm.CloseEvent.class, e->closeEditor());
     }
 
-    private void attachImageUpload(Upload upload, Image preview) {
-        ByteArrayOutputStream uploadBuffer = new ByteArrayOutputStream();
-        upload.setAcceptedFileTypes("image/*");
-        upload.setReceiver((fileName, mimeType) -> {
-            return uploadBuffer;
-        });
-        upload.addSucceededListener(e -> {
-            String mimeType = e.getMIMEType();
-            String base64ImageData = Base64.getEncoder().encodeToString(uploadBuffer.toByteArray());
-            String dataUrl = "data:" + mimeType + ";base64,"
-                    + UriUtils.encodeQuery(base64ImageData, StandardCharsets.UTF_8);
-            upload.getElement().setPropertyJson("files", Json.createArray());
-            preview.setSrc(dataUrl);
-            uploadBuffer.reset();
-        });
-        preview.setVisible(false);
+    private void saveSkiresort(SkiResortForm.SaveEvent event){
+        service.update(event.getContact());
+        Notification.show("Skigebiet gesichert");
+        updateList();
+        closeEditor();
     }
 
-    private void refreshGrid() {
-        grid.select(null);
-        grid.getLazyDataView().refreshAll();
+    private void deleteSkiresort(SkiResortForm.DeleteEvent event){
+        service.delete(event.getContact().getId());
+        Notification.show("Skigebiet gelöscht");
+        updateList();
+        closeEditor();
     }
 
-    private void clearForm() {
-        populateForm(null);
+    private void configureGrid() {
+
+        grid.addClassNames("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns();
+        grid.addColumn(SkiResort::getName).setHeader("Name");
+        grid.addColumn(SkiResort::getRegion).setHeader("Region");
+        grid.addColumn(SkiResort::getOperator).setHeader("Betreiber");
+        grid.addColumn(SkiResort::getAddress).setHeader("Addresse");
+        grid.addColumn(SkiResort::getZip).setHeader("Postleitzahl");
+        grid.addColumn(SkiResort::getCity).setHeader("Stadt");
+        grid.addColumn(SkiResort::getHeightMin).setHeader("Schneehöhe min");
+        grid.addColumn(SkiResort::getHeightMax).setHeader("Schneehöhe max");
+        grid.addColumn(SkiResort::getTotalLength).setHeader("Streckenlänge");
+        grid.addColumn(SkiResort::getRopeways).setHeader("Pistenanzahl");
+        grid.addColumn(SkiResort::getPosLon).setHeader("Breitengrad");
+        grid.addColumn(SkiResort::getPosLat).setHeader("Längengrad");
+        grid.addColumn(SkiResort::getDateSeasonStart).setHeader("Start-Saison");
+        grid.addColumn(SkiResort::getDateSeasonEnd).setHeader("Ende-Saison");
+        grid.addColumn(SkiResort::getTimeServiceStart).setHeader("Öffnungszeiten");
+        grid.addColumn(SkiResort::getTimeServiceEnd).setHeader("Schließzeiten");
+        grid.addColumn(SkiResort::getURLImageSlope).setHeader("Pistenbild");
+        grid.addColumn(SkiResort::getURLImageFront).setHeader("Titelbild");
+        grid.addColumn(SkiResort::getURLTicketpage).setHeader("Ticketseite");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+        grid.asSingleSelect().addValueChangeListener(e -> editContact(e.getValue()));
     }
 
-    private void populateForm(SampleBook value) {
-        this.sampleBook = value;
-        binder.readBean(this.sampleBook);
-        this.imagePreview.setVisible(value != null);
-        if (value == null) {
-            this.imagePreview.setSrc("");
-        } else {
-            this.imagePreview.setSrc(value.getImage());
+    private void editContact(SkiResort skiResort){
+        if(skiResort == null){
+            closeEditor();
+        }else {
+            form.setSkiResort(skiResort);
+            form.setVisible(true);
+            addClassName("editing");
         }
+    }
+
+    private HorizontalLayout getToolbar() {
+
+        Button addSkiButton = new Button("Skigebiet hinzufügen");
+        addSkiButton.addClickListener(e->addSkiResort());
+        HorizontalLayout toolbar = new HorizontalLayout(addSkiButton);
+
+        toolbar.addClassName("toolbar");
+        return toolbar;
 
     }
+    private void addSkiResort(){
+        grid.asSingleSelect().clear();
+        editContact(new SkiResort());
+    }
+
+
 }
+
