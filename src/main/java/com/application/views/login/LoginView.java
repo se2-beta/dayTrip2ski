437aa854c6dc81f73f524e9bafa47c2efd421625
LukasG <gl1935@mci4me.at>
2022-06-19 15:04:39 +0200

    cleanup

--- src/main/java/com/application/views/login/LoginView.java
+++ src/main/java/com/application/views/login/LoginView.java
@@ -5,13 +5,9 @@ import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.StreamResource;
 
 @PageTitle("Login")
 @Route(value = "login")
