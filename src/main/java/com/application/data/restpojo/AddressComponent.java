1788160fa078a6aaacf4b28db932e6b46a2dc0f5
hu4361 <hu4361@mci4me.at>
2022-06-20 13:18:45 +0200

    added new POJO

---
+++ src/main/java/com/application/data/restpojo/AddressComponent.java
@@ -0,0 +1,56 @@
+
+package com.application.data.restpojo;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "long_name",
+    "short_name",
+    "types"
+})
+@Generated("jsonschema2pojo")
+public class AddressComponent {
+
+    @JsonProperty("long_name")
+    private String longName;
+    @JsonProperty("short_name")
+    private String shortName;
+    @JsonProperty("types")
+    private List<String> types = null;
+
+    @JsonProperty("long_name")
+    public String getLongName() {
+        return longName;
+    }
+
+    @JsonProperty("long_name")
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    @JsonProperty("short_name")
+    public String getShortName() {
+        return shortName;
+    }
+
+    @JsonProperty("short_name")
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    @JsonProperty("types")
+    public List<String> getTypes() {
+        return types;
+    }
+
+    @JsonProperty("types")
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+}
