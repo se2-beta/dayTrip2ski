d28d8d424f6cdaba5831cb09c6ef117a8869ac65
hu4361 <hu4361@mci4me.at>
2022-06-22 13:19:30 +0200

    updated imports

--- src/main/java/com/application/data/service/SkiResortService.java
+++ src/main/java/com/application/data/service/SkiResortService.java
@@ -5,6 +5,7 @@ import com.application.data.restpojo.DataDay;
 import com.application.data.restpojo.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
