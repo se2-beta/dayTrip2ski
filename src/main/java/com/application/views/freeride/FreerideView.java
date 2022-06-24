0c64a47c24665529db8fec9b8cbf3cd0f3c24f69
LukasG <gl1935@mci4me.at>
2022-06-24 11:47:23 +0200

    changes to gif, added "redirect to SkiResortListView" button

--- src/main/java/com/application/views/freeride/FreerideView.java
+++ src/main/java/com/application/views/freeride/FreerideView.java
@@ -1,12 +1,18 @@
 package com.application.views.freeride;
 
 import com.application.views.MainLayout;
+import com.application.views.imagelist.SkiResortDetailView;
+import com.application.views.imagelist.SkiResortListView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteParameters;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @PageTitle("Freeride")
@@ -18,9 +24,9 @@ public class FreerideView extends VerticalLayout {
         setSpacing(false);
 
         Image img = new Image();
-        img.setSrc("https://media0.giphy.com/media/YoYOhif8otaJI8uIMT/giphy.gif");
-        img.setWidth("400px");
-        img.setHeight("400px");
+        img.setSrc("https://media0.giphy.com/media/kTPUu2dXsWu4YmcxWz/200w.gif?cid=82a1493bupy9boa59iugssof6bqejpbli496p5uw7i6k3pcq&rid=200w.gif&ct=g");
+        img.setWidth("200px");
+        img.setHeight("200px");
         add(img);
 
         Paragraph lawText = new Paragraph("""
@@ -31,12 +37,12 @@ public class FreerideView extends VerticalLayout {
                 Abfahrens mit Alpinschiern, ein. Der zweite Satz des
                 § 33 Abs. 3 verbietet nun das „Abfahren mit Schiern
                 im Wald... im Bereich von Aufstiegshilfen“ außerhalb
-                von markierten Pisten oder Skirouten. Die Bestimmung soll verhindern, 
+                von markierten Pisten oder Skirouten. Die Bestimmung soll verhindern,
                 dass Benützer von Aufstiegshilfen mehrmals am Tag statt auf den erschlossenen
                 Pisten (oder Skirouten) „zur Abwechslung“ durch
                 den Wald abfahren, also das typische „Variantenfahren“. Wieweit der „Bereich von Aufstiegshilfen“
                 reicht, für den dieses Verbot gilt, ist im einzelnen
-                etwas strittig, die obigen umschriebene „Variante“ 
+                etwas strittig, die obigen umschriebene „Variante“
                 fällt jedenfalls auch bei großzügigem Verständnis dieses Begriffs darunter. Gerade auf Varianten
                 in dem hier verwendeten Sinn ist also das Abfahren mit Skiern im Wald verboten. Eine Übertretung
                 dieses Verbots ist nicht nur mit den zivilrechtlichen
@@ -47,8 +53,25 @@ public class FreerideView extends VerticalLayout {
                 Verwaltungsstrafe.""");
         lawText.setWidth("80%");
 
-        add(new H2("Freeriden ist nur unter bestimmten Bedingungen erlaubt!"));
-        add(lawText);
+        Paragraph information = new Paragraph("""
+                Sollten Sie dennoch Informationen zu Wetterbedingungen, Schneefall, usw. in den jeweiligen Skigebieten benötigen,
+                dann können Sie diese gerne unter dem Menüpunkt "Skigebiete" abfragen.
+                """);
+        information.getElement().getStyle().set("font-weight", "bold");
+        information.setWidth("80%");
+
+
+        Button redirect = new Button("weiterleiten zu Skigebiete");
+        redirect.addClickListener(e -> UI.getCurrent().navigate(SkiResortListView.class)
+        );
+
+
+        add(
+                new H2("Freeriden ist nur unter bestimmten Bedingungen erlaubt!"),
+                lawText,
+                information,
+                redirect
+        );
 
         setSizeFull();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
