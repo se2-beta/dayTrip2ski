b873d50616bced595c710859dbc507e2ed16fe76
Lukas-Gerber <71282814+Lukas-Gerber@users.noreply.github.com>
2022-06-24 16:41:26 +0200

    About view frontend (#21)
    * added teamimage & description template, added some teammember pictures
    
    * added AboutView text, added pictures of Markus and Andreas

--- src/main/java/com/application/views/about/AboutView.java
+++ src/main/java/com/application/views/about/AboutView.java
@@ -1,9 +1,9 @@
 package com.application.views.about;
 
 import com.application.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,19 +15,105 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 public class AboutView extends VerticalLayout {
 
     public AboutView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        setSpacing(false);
+
+
+        H1 aboutTitle = new H1("About");
+        aboutTitle.addClassNames("pb-s", "mt-0", "pt-m");
+
+        Image logo = new Image("images/daytrip2Ski_logo.png", "logo");
+        logo.setMaxWidth("150px");
+        logo.setMaxHeight("150px");
+
+        VerticalLayout title = new VerticalLayout(logo, aboutTitle);
+        title.setSpacing(false);
+        title.setWidthFull();
+        title.setJustifyContentMode(JustifyContentMode.CENTER);
+        title.setAlignItems(Alignment.CENTER);
+
+        Label aboutText = new Label("Mithilfe der dayTrip2Ski-App wird es Ihnen ermöglicht, das ideale Skigebiet für ihre Ansprüche zu finden. \n" +
+                "Geben sie hierfür in der Skigebiete-Ansicht einfach ihre Präferenzen an und der ausgeklügelte Algorithmus berechnet ihnen ihr Traum-Skigebiet. " +
+                "Natürlich können Sie auch nach einem Skigebiet sowohl in der Kartenansicht als auch in der Liste suchen.");
+        aboutText.setMaxWidth("80%");
+
+        HorizontalLayout aboutLayout = new HorizontalLayout(aboutText);
+        aboutLayout.setWidthFull();
+        aboutLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        aboutLayout.setAlignItems(Alignment.CENTER);
+
+        H1 titleText = new H1("Team Beta");
+        titleText.addClassNames("pb-s");
+
+        HorizontalLayout teamTitle = new HorizontalLayout(titleText);
+        teamTitle.setWidthFull();
+        teamTitle.setJustifyContentMode(JustifyContentMode.CENTER);
+        teamTitle.setAlignItems(Alignment.CENTER);
+
+
+        VerticalLayout mainLayout = new VerticalLayout();
+        mainLayout.setHeightFull();
+        mainLayout.setSpacing(false);
+        mainLayout.setJustifyContentMode(JustifyContentMode.AROUND);
+        mainLayout.add(
+                threeMemberLayout(
+                        teamMember("images/Annina_Ecker.jpg", "Annina Ecker", "Build Engineer"),
+                        teamMember("images/Lukas_Gerber.jpg", "Lukas Gerber", "Frontend Dev"),
+                        teamMember("images/Markus_Russold_ws.jpg", "Markus Russold", "Backend Dev")
+
+                ),
+                threeMemberLayout(
+                        teamMember("images/David_Zollitsch_ws.jpg", "David Zollitsch", "Quality Engineer"),
+                        teamMember("images/Andreas_Schoffel.jpg", "Andreas Schöffel", "Frontend Dev"),
+                        teamMember("images/Julian_Hirschberger_ws.jpg", "Julian Hirschberger", "Backend Dev")
+                )
+        );
+
+        add(
+                title,
+                aboutLayout,
+                teamTitle,
+                mainLayout
+        );
+    }
+
+    private Component teamMember(String image, String fullname, String role) {
+
+        Image img = new Image();
+        img.setSrc(image);
+        img.setWidth("100px");
+        img.setHeight("100px");
+        img.getElement().getStyle().set("border-radius", "50%").set("object-fit", "cover");
+
+
+        H3 name = new H3(fullname);
+        name.addClassNames("m-0");
+        Label position = new Label(role);
+
+        VerticalLayout namePosition = new VerticalLayout(name, position);
+        namePosition.setSpacing(false);
+        namePosition.addClassNames("p-0", "m-0");
+        namePosition.setAlignItems(Alignment.CENTER);
+
+        VerticalLayout layout = new VerticalLayout(img, namePosition);
+        layout.addClassNames("pt-m", "pl-m");
+        layout.setWidth("33%");
+        layout.setAlignItems(Alignment.CENTER);
+        return layout;
+    }
+
+    private Component threeMemberLayout(Component member1, Component member2, Component member3) {
+
+        HorizontalLayout layout = new HorizontalLayout(
+                member1,
+                member2,
+                member3
+        );
+        layout.setWidthFull();
+        layout.setJustifyContentMode(JustifyContentMode.BETWEEN);
+
+        return layout;
     }
 
 }
