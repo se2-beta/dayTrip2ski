1fb991262133f6b66537a1f88fc4b9dcbfa375ea
Markus Russold <rm4870@mci4me.at>
2022-06-26 10:04:36 +0200

    Adapted RatingServiceTest to test multiple ratings per a single SkiResort

--- src/test/java/com/application/data/service/RatingServiceTest.java
+++ src/test/java/com/application/data/service/RatingServiceTest.java
@@ -14,96 +14,119 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class RatingServiceTest {
-//    @Autowired
-//    RatingService ratingService;
-//    @Autowired
-//    RatingRepository repository;
-//    @Autowired
-//    UserRepository userRepository;
-//    @Autowired
-//    SkiResortRepository skiResortRepository;
-//    Logger logger;
-//
-//    public RatingServiceTest() {
-//        this.logger = LoggerFactory.getLogger(getClass());
-//    }
-//
-//
-//    @Test
-//    public void CreateAndGet() {
-//        this.logger.info("start ...");
-//
-//        User testUser = new User();
-//        testUser.setName("TestName");
-//        testUser.setUsername("usertest");
-//        testUser.setProfilePictureUrl("http://...");
-//        testUser.setHomeLat(1.0);
-//        testUser.setHomeLon(2.0);
-//        userRepository.save(testUser);
-//
-//        SkiResort testsSkiResort = new SkiResort();
-//        testsSkiResort.setName("MyTest");
-//        testsSkiResort.setRegion("Steiermark");
-//        testsSkiResort.setOperator("Planai-Hochwurzen-Bahnen Gesellschaft m.b.H.");
-//        testsSkiResort.setAddress("Coburgstrasse 52");
-//        testsSkiResort.setZip(8970);
-//        testsSkiResort.setCity("Schladming");
-//        testsSkiResort.setHeightMin(268);
-//        testsSkiResort.setHeightMax(1906);
-//        testsSkiResort.setTotalLength(122.7);
-//        testsSkiResort.setRopeways(14);
-//        testsSkiResort.setPosLon(13.6785045);
-//        testsSkiResort.setPosLat(47.3901116);
-//        testsSkiResort.setDateSeasonStart("01.10.2022");
-//        testsSkiResort.setDateSeasonEnd("31.03.2023");
-//        testsSkiResort.setTimeServiceStart("09:00");
-//        testsSkiResort.setTimeServiceEnd("16.00");
-//        testsSkiResort.setCurrentUtilizationPercent(65);
-//        testsSkiResort.setUserRating(90);
-//        testsSkiResort.setWeatherCurrentWindspeed(23.50);
-//        testsSkiResort.setWeatherCurrentTemperature(25.80);
-//        testsSkiResort.setWeatherCurrentSnowfallForecastPercent(70);
-//        testsSkiResort.setWeatherCurrentSnowfallForecastAmountMM(20);
-//        testsSkiResort.setWeatherDatetimeLastRead("05.06.2022 16:05:14");
-//        testsSkiResort.setSnowDepthMin(10);
-//        testsSkiResort.setSnowDepthMax(35);
-//        testsSkiResort.setAmountFreshSnow(40);
-//        testsSkiResort.setDateLastSnowfall("04.06.2022");
-//        testsSkiResort.setURLTicketpage("https://www.planai.at/de/tickets-preise/preise-winter");
-//        testsSkiResort.setAvalancheWarningLevel(2);
-//        testsSkiResort.setURLImageFront("https://urlaubsgeschichten.at/wp-content/uploads/2019/03/schladming-planai-20.jpg");
-//        testsSkiResort.setURLImageSlope("https://hikeandbike.de/wp-content/uploads/2014/07/Pistenplan-Planai.jpg");
-//        skiResortRepository.save(testsSkiResort);
-//
-//        ratingService.setRating(testUser, testsSkiResort, 1.5, "45", 45.0, "100", 100.0);
-//
-//        Optional<Rating> testRating = ratingService.get(testUser, testsSkiResort);
-//        if (testRating != null) {
-//            if (testRating.isPresent()) {
-//                Assert.assertEquals(testRating.get().getUser().getName(), "TestName");
-//                Assert.assertEquals(testRating.get().getSkiResort().getName(), "MyTest");
-//                Assert.assertEquals(testRating.get().getRating(), 1.5, 0);
-//                Assert.assertEquals(testRating.get().getDistanceStr(), "45");
-//                Assert.assertEquals(testRating.get().getDistanceVal(), 45.0, 0);
-//                Assert.assertEquals(testRating.get().getDurationStr(), "100");
-//                Assert.assertEquals(testRating.get().getDurationVal(), 100.0, 0);
-//            } else {
-//                Assert.assertTrue(testRating.isPresent());
-//            }
-//        } else {
-//            Assert.assertNotEquals(testRating, null);
-//        }
-//
-//        this.logger.info("... end");
-//    }
-//
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RatingServiceTest {
+    @Autowired
+    RatingService ratingService;
+    @Autowired
+    RatingRepository repository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    SkiResortRepository skiResortRepository;
+    @Autowired
+    SkiResortService skiResortservice;
+    Logger logger;
+
+    public RatingServiceTest() {
+        this.logger = LoggerFactory.getLogger(getClass());
+    }
+
+
+    @Test
+    public void CreateAndGet() {
+
+
+        this.logger.info("start ...");
+
+        User testUser = new User();
+        testUser.setName("TestName");
+        testUser.setUsername("usertest");
+        testUser.setProfilePictureUrl("http://...");
+        testUser.setHomeLat(1.0);
+        testUser.setHomeLon(2.0);
+        userRepository.save(testUser);
+
+        User testUser2 = new User();
+        testUser2.setName("TestName2");
+        testUser2.setUsername("usertest2");
+        testUser2.setProfilePictureUrl("http://...");
+        testUser2.setHomeLat(1.0);
+        testUser2.setHomeLon(2.0);
+        userRepository.save(testUser2);
+
+        SkiResort testsSkiResort = new SkiResort();
+        testsSkiResort.setName("MyTest");
+        testsSkiResort.setRegion("Steiermark");
+        testsSkiResort.setOperator("Planai-Hochwurzen-Bahnen Gesellschaft m.b.H.");
+        testsSkiResort.setAddress("Coburgstrasse 52");
+        testsSkiResort.setZip(8970);
+        testsSkiResort.setCity("Schladming");
+        testsSkiResort.setHeightMin(268);
+        testsSkiResort.setHeightMax(1906);
+        testsSkiResort.setTotalLength(122.7);
+        testsSkiResort.setRopeways(14);
+        testsSkiResort.setPosLon(13.6785045);
+        testsSkiResort.setPosLat(47.3901116);
+        testsSkiResort.setDateSeasonStart("01.10.2022");
+        testsSkiResort.setDateSeasonEnd("31.03.2023");
+        testsSkiResort.setTimeServiceStart("09:00");
+        testsSkiResort.setTimeServiceEnd("16.00");
+        testsSkiResort.setCurrentUtilizationPercent(65);
+        testsSkiResort.setUserRating(90);
+        testsSkiResort.setWeatherCurrentWindspeed(23.50);
+        testsSkiResort.setWeatherCurrentTemperature(25.80);
+        testsSkiResort.setWeatherCurrentSnowfallForecastPercent(70);
+        testsSkiResort.setWeatherCurrentSnowfallForecastAmountMM(20);
+        testsSkiResort.setWeatherDatetimeLastRead("05.06.2022 16:05:14");
+        testsSkiResort.setSnowDepthMin(10);
+        testsSkiResort.setSnowDepthMax(35);
+        testsSkiResort.setAmountFreshSnow(40);
+        testsSkiResort.setDateLastSnowfall("04.06.2022");
+        testsSkiResort.setURLTicketpage("https://www.planai.at/de/tickets-preise/preise-winter");
+        testsSkiResort.setAvalancheWarningLevel(2);
+        testsSkiResort.setURLImageFront("https://urlaubsgeschichten.at/wp-content/uploads/2019/03/schladming-planai-20.jpg");
+        testsSkiResort.setURLImageSlope("https://hikeandbike.de/wp-content/uploads/2014/07/Pistenplan-Planai.jpg");
+        skiResortRepository.save(testsSkiResort);
+
+        ratingService.setRating(testUser, testsSkiResort, 1.5, "45", 45.0, "100", 100.0);
+        ratingService.setRating(testUser2, testsSkiResort, 5.0, "45", 45.0, "100", 100.0);
+
+        Optional<Rating> testRating = ratingService.get(testUser, testsSkiResort);
+        if (testRating != null) {
+            if (testRating.isPresent()) {
+                Assert.assertEquals(testRating.get().getUser().getName(), "TestName");
+                Assert.assertEquals(testRating.get().getSkiResort().getName(), "MyTest");
+                Assert.assertEquals(testRating.get().getRating(), 1.5, 0);
+                Assert.assertEquals(testRating.get().getDistanceStr(), "45");
+                Assert.assertEquals(testRating.get().getDistanceVal(), 45.0, 0);
+                Assert.assertEquals(testRating.get().getDurationStr(), "100");
+                Assert.assertEquals(testRating.get().getDurationVal(), 100.0, 0);
+            } else {
+                Assert.assertTrue(testRating.isPresent());
+            }
+        } else {
+            Assert.assertNotEquals(testRating, null);
+        }
+
+        Optional<SkiResort> testResort = skiResortservice.get("MyTest");
+        if (testResort.isPresent()) {
+            List<Rating> ratingList = testResort.get().getRatings();
+
+            for (int i = 0; i < ratingList.size(); i++) {
+                this.logger.info("" + ratingList.get(i).getRating());
+            }
+        }
+
+        this.logger.info("... end");
+    }
+
 //    @Test
 //    public void callRest() {
 //        this.logger.info("start ...");
@@ -136,7 +159,7 @@ import static org.junit.jupiter.api.Assertions.*;
 //
 //        this.logger.info("... end");
 //    }
-
+//
 //    @Test
 //    public void callRest() {
 //        this.logger.info("start ...");
@@ -157,4 +180,4 @@ import static org.junit.jupiter.api.Assertions.*;
 //        this.logger.info(String.valueOf(testUser.getId()));
 //
 //    }
-//}
+}
