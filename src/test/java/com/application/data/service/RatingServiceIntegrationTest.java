81b5430e75114711ae8dd5d974c65dbbae81c9a6
David Zollitsch <97090482+Mips123@users.noreply.github.com>
2022-07-04 18:51:21 +0200

    Cleanup + Abgabefiles added (#42)

--- src/test/java/com/application/data/service/RatingServiceIntegrationTest.java
+++ src/test/java/com/application/data/service/RatingServiceIntegrationTest.java
@@ -30,72 +30,14 @@ public class RatingServiceIntegrationTest {
 
     @Test
     public void setRating() {
-        User testUser = new User();
-
+        User testUser = createUser();
         String username = "Theo";
-        String name = "Horst";
-        String profilPictureUrl = "asdf";
-        Double homeLat = 46.00;
-        Double homeLon = 46.00;
-
         testUser.setUsername(username);
-        testUser.setName(name);
-        testUser.setProfilePictureUrl(profilPictureUrl);
-        testUser.setHomeLat(homeLat);
-        testUser.setHomeLon(homeLon);
-
         userRepository.save(testUser);
 
-        SkiResort skiResort1 = new SkiResort();
-
+        SkiResort skiResort1 = createSkiResort();
         String resortName = "RatingResort";
-        String region = "Tirol";
-        String operator = "Fake Gmbh";
-        String address = "Talstraße1";
-        Integer zip = 12345;
-        String city = "Fakecity";
-        Integer heightMin = 0;
-        Integer heightMax = 1000;
-        Double totalLength = 10.00;
-        Integer ropeways = 10;
-        String dateSeasonStart = "asdf";
-        String dateSeasonEnd  = "asdf";
-        String timeServiceStart  = "asdf";
-        String timeServiceEnd  = "asdf";
-        Integer currentUtilizationPercent = 1;
-        Integer snowDepthMin  = 1;
-        Integer snowDepthMax  = 1;
-        Integer amountFreshSnow = 1;
-        String dateLastSnowfall = "asdf";
-        String URLTicketpage = "asdf";
-        Integer avalancheWarningLevel = 1;
-        String URLImageFront = "asdf";
-        String URLImageSlope = "asdf";
-
         skiResort1.setName(resortName);
-        skiResort1.setRegion(region);
-        skiResort1.setOperator(operator);
-        skiResort1.setAddress(address);
-        skiResort1.setZip(zip);
-        skiResort1.setCity(city);
-        skiResort1.setHeightMin(heightMin);
-        skiResort1.setHeightMax(heightMax);
-        skiResort1.setTotalLength(totalLength);
-        skiResort1.setRopeWays(ropeways);
-        skiResort1.setDateSeasonStart(dateSeasonStart);
-        skiResort1.setDateSeasonEnd(dateSeasonEnd);
-        skiResort1.setTimeServiceStart(timeServiceStart);
-        skiResort1.setTimeServiceEnd(timeServiceEnd);
-        skiResort1.setCurrentUtilizationPercent(currentUtilizationPercent);
-        skiResort1.setSnowDepthMin(snowDepthMin);
-        skiResort1.setSnowDepthMax(snowDepthMax);
-        skiResort1.setAmountFreshSnow(amountFreshSnow);
-        skiResort1.setDateLastSnowfall(dateLastSnowfall);
-        skiResort1.setUrlTicketPage(URLTicketpage);
-        skiResort1.setAvalancheWarningLevel(avalancheWarningLevel);
-        skiResort1.setUrlImageFront(URLImageFront);
-        skiResort1.setUrlImageSlope(URLImageSlope);
-
         skiResortRepository.save(skiResort1);
 
         ratingService.setRating(
@@ -122,73 +64,14 @@ public class RatingServiceIntegrationTest {
 
     @Test
     public void getRatingByRatingId() {
-        User testUser = new User();
-
+        User testUser = createUser();
         String username = "Gerry";
-        String name = "Horst";
-        String profilPictureUrl = "asdf";
-        Double homeLat = 46.00;
-        Double homeLon = 46.00;
-
         testUser.setUsername(username);
-        testUser.setName(name);
-        testUser.setProfilePictureUrl(profilPictureUrl);
-        testUser.setHomeLat(homeLat);
-        testUser.setHomeLon(homeLon);
-
         userRepository.save(testUser);
 
-        SkiResort skiResort1 = new SkiResort();
-        //Optional<SkiResort> skiResort1 = Optional.of(new SkiResort());
-
+        SkiResort skiResort1 = createSkiResort();
         String resortName = "getRatingResort";
-        String region = "Tirol";
-        String operator = "Fake Gmbh";
-        String address = "Talstraße1";
-        Integer zip = 12345;
-        String city = "Fakecity";
-        Integer heightMin = 0;
-        Integer heightMax = 1000;
-        Double totalLength = 10.00;
-        Integer ropeways = 10;
-        String dateSeasonStart = "asdf";
-        String dateSeasonEnd  = "asdf";
-        String timeServiceStart  = "asdf";
-        String timeServiceEnd  = "asdf";
-        Integer currentUtilizationPercent = 1;
-        Integer snowDepthMin  = 1;
-        Integer snowDepthMax  = 1;
-        Integer amountFreshSnow = 1;
-        String dateLastSnowfall = "asdf";
-        String URLTicketpage = "asdf";
-        Integer avalancheWarningLevel = 1;
-        String URLImageFront = "asdf";
-        String URLImageSlope = "asdf";
-
         skiResort1.setName(resortName);
-        skiResort1.setRegion(region);
-        skiResort1.setOperator(operator);
-        skiResort1.setAddress(address);
-        skiResort1.setZip(zip);
-        skiResort1.setCity(city);
-        skiResort1.setHeightMin(heightMin);
-        skiResort1.setHeightMax(heightMax);
-        skiResort1.setTotalLength(totalLength);
-        skiResort1.setRopeWays(ropeways);
-        skiResort1.setDateSeasonStart(dateSeasonStart);
-        skiResort1.setDateSeasonEnd(dateSeasonEnd);
-        skiResort1.setTimeServiceStart(timeServiceStart);
-        skiResort1.setTimeServiceEnd(timeServiceEnd);
-        skiResort1.setCurrentUtilizationPercent(currentUtilizationPercent);
-        skiResort1.setSnowDepthMin(snowDepthMin);
-        skiResort1.setSnowDepthMax(snowDepthMax);
-        skiResort1.setAmountFreshSnow(amountFreshSnow);
-        skiResort1.setDateLastSnowfall(dateLastSnowfall);
-        skiResort1.setUrlTicketPage(URLTicketpage);
-        skiResort1.setAvalancheWarningLevel(avalancheWarningLevel);
-        skiResort1.setUrlImageFront(URLImageFront);
-        skiResort1.setUrlImageSlope(URLImageSlope);
-
         skiResortRepository.save(skiResort1);
 
         ratingService.setRating(
@@ -215,73 +98,14 @@ public class RatingServiceIntegrationTest {
 
     @Test
     public void getRatingByUserAndSkiResort() {
-        User testUser = new User();
-
+        User testUser = createUser();
         String username = "Jerry";
-        String name = "Horst";
-        String profilPictureUrl = "asdf";
-        Double homeLat = 46.00;
-        Double homeLon = 46.00;
-
         testUser.setUsername(username);
-        testUser.setName(name);
-        testUser.setProfilePictureUrl(profilPictureUrl);
-        testUser.setHomeLat(homeLat);
-        testUser.setHomeLon(homeLon);
-
         userRepository.save(testUser);
 
-        SkiResort skiResort1 = new SkiResort();
-        //Optional<SkiResort> skiResort1 = Optional.of(new SkiResort());
-
+        SkiResort skiResort1 = createSkiResort();
         String resortName = "FirstRatingResort";
-        String region = "Tirol";
-        String operator = "Fake Gmbh";
-        String address = "Talstraße1";
-        Integer zip = 12345;
-        String city = "Fakecity";
-        Integer heightMin = 0;
-        Integer heightMax = 1000;
-        Double totalLength = 10.00;
-        Integer ropeways = 10;
-        String dateSeasonStart = "asdf";
-        String dateSeasonEnd  = "asdf";
-        String timeServiceStart  = "asdf";
-        String timeServiceEnd  = "asdf";
-        Integer currentUtilizationPercent = 1;
-        Integer snowDepthMin  = 1;
-        Integer snowDepthMax  = 1;
-        Integer amountFreshSnow = 1;
-        String dateLastSnowfall = "asdf";
-        String URLTicketpage = "asdf";
-        Integer avalancheWarningLevel = 1;
-        String URLImageFront = "asdf";
-        String URLImageSlope = "asdf";
-
         skiResort1.setName(resortName);
-        skiResort1.setRegion(region);
-        skiResort1.setOperator(operator);
-        skiResort1.setAddress(address);
-        skiResort1.setZip(zip);
-        skiResort1.setCity(city);
-        skiResort1.setHeightMin(heightMin);
-        skiResort1.setHeightMax(heightMax);
-        skiResort1.setTotalLength(totalLength);
-        skiResort1.setRopeWays(ropeways);
-        skiResort1.setDateSeasonStart(dateSeasonStart);
-        skiResort1.setDateSeasonEnd(dateSeasonEnd);
-        skiResort1.setTimeServiceStart(timeServiceStart);
-        skiResort1.setTimeServiceEnd(timeServiceEnd);
-        skiResort1.setCurrentUtilizationPercent(currentUtilizationPercent);
-        skiResort1.setSnowDepthMin(snowDepthMin);
-        skiResort1.setSnowDepthMax(snowDepthMax);
-        skiResort1.setAmountFreshSnow(amountFreshSnow);
-        skiResort1.setDateLastSnowfall(dateLastSnowfall);
-        skiResort1.setUrlTicketPage(URLTicketpage);
-        skiResort1.setAvalancheWarningLevel(avalancheWarningLevel);
-        skiResort1.setUrlImageFront(URLImageFront);
-        skiResort1.setUrlImageSlope(URLImageSlope);
-
         skiResortRepository.save(skiResort1);
 
         ratingService.setRating(
@@ -307,87 +131,18 @@ public class RatingServiceIntegrationTest {
 
     @Test
     public void checkIfRatingGotCalculated(){
-        User testUser = new User();
-
+        User testUser = createUser();
         String username = "Johny";
-        String name = "Horst";
-        String profilPictureUrl = "asdf";
-        Double homeLat = 46.00;
-        Double homeLon = 46.00;
-
-        Integer weightFreshSnow = 2;
-        Integer weightSlopeLength = 3;
-        Integer weightTravelTime = 4;
-        Integer weightOccupancy = 5;
-
         testUser.setUsername(username);
-        testUser.setName(name);
-        testUser.setProfilePictureUrl(profilPictureUrl);
-        testUser.setHomeLat(homeLat);
-        testUser.setHomeLon(homeLon);
-
-        testUser.setWeightFreshSnow(weightFreshSnow);
-        testUser.setWeightSlopeLength(weightSlopeLength);
-        testUser.setWeightTravelTime(weightTravelTime);
-        testUser.setWeightOccupancy(weightOccupancy);
-
         userRepository.save(testUser);
 
-        SkiResort skiResort1 = new SkiResort();
-
+        SkiResort skiResort1 = createSkiResort();
         String resortName = "SecondRatingResort";
-        String region = "Tirol";
-        String operator = "Fake Gmbh";
-        String address = "Talstraße1";
-        Integer zip = 12345;
-        String city = "Fakecity";
-        Integer heightMin = 0;
-        Integer heightMax = 1000;
-        Double totalLength = 10.00;
-        Integer ropeways = 10;
-        String dateSeasonStart = "asdf";
-        String dateSeasonEnd  = "asdf";
-        String timeServiceStart  = "asdf";
-        String timeServiceEnd  = "asdf";
-        Integer currentUtilizationPercent = 1;
-        Integer snowDepthMin  = 1;
-        Integer snowDepthMax  = 1;
-        Integer amountFreshSnow = 1;
-        String dateLastSnowfall = "asdf";
-        String URLTicketpage = "asdf";
-        Integer avalancheWarningLevel = 1;
-        String URLImageFront = "asdf";
-        String URLImageSlope = "asdf";
-
         skiResort1.setName(resortName);
-        skiResort1.setRegion(region);
-        skiResort1.setOperator(operator);
-        skiResort1.setAddress(address);
-        skiResort1.setZip(zip);
-        skiResort1.setCity(city);
-        skiResort1.setHeightMin(heightMin);
-        skiResort1.setHeightMax(heightMax);
-        skiResort1.setTotalLength(totalLength);
-        skiResort1.setRopeWays(ropeways);
-        skiResort1.setDateSeasonStart(dateSeasonStart);
-        skiResort1.setDateSeasonEnd(dateSeasonEnd);
-        skiResort1.setTimeServiceStart(timeServiceStart);
-        skiResort1.setTimeServiceEnd(timeServiceEnd);
-        skiResort1.setCurrentUtilizationPercent(currentUtilizationPercent);
-        skiResort1.setSnowDepthMin(snowDepthMin);
-        skiResort1.setSnowDepthMax(snowDepthMax);
-        skiResort1.setAmountFreshSnow(amountFreshSnow);
-        skiResort1.setDateLastSnowfall(dateLastSnowfall);
-        skiResort1.setUrlTicketPage(URLTicketpage);
-        skiResort1.setAvalancheWarningLevel(avalancheWarningLevel);
-        skiResort1.setUrlImageFront(URLImageFront);
-        skiResort1.setUrlImageSlope(URLImageSlope);
-
         skiResortRepository.save(skiResort1);
 
         Optional<Rating> testRating = ratingService.get(testUser, skiResort1);
 
-        //Check if there is a rating for this combination of User and skiresort
         Assert.assertTrue(testRating.isEmpty());
 
         ratingService.calculateRating(testUser, skiResort1);
@@ -402,7 +157,6 @@ public class RatingServiceIntegrationTest {
             throw new NoSuchElementException("There is no rating regarding this combination");
         }
 
-        //check if rating got updated
         Integer weightFreshSnow_new = 10;
         testUser.setWeightFreshSnow(weightFreshSnow_new);
 
@@ -419,106 +173,26 @@ public class RatingServiceIntegrationTest {
         }
     }
 
-    //01.07.22 Test funktioniert nicht
     @Test
     public void checkIfAllRatingsAreCalculated(){
-        User testUser = new User();
+        User testUser = createUser();
 
         String username = "Isa";
-        String name = "Horst";
-        String profilPictureUrl = "asdf";
-        Double homeLat = 46.00;
-        Double homeLon = 46.00;
-
-        Integer weightFreshSnow = 2;
-        Integer weightSlopeLength = 3;
-        Integer weightTravelTime = 4;
-        Integer weightOccupancy = 5;
-
         testUser.setUsername(username);
-        testUser.setName(name);
-        testUser.setProfilePictureUrl(profilPictureUrl);
-        testUser.setHomeLat(homeLat);
-        testUser.setHomeLon(homeLon);
-
-        testUser.setWeightFreshSnow(weightFreshSnow);
-        testUser.setWeightSlopeLength(weightSlopeLength);
-        testUser.setWeightTravelTime(weightTravelTime);
-        testUser.setWeightOccupancy(weightOccupancy);
-
         userRepository.save(testUser);
 
-        User testUser1 = new User();
-
+        User testUser1 = createUser();
         String username1 = "Leo";
-
         Integer weightOccupancy1 = 10;
-
         testUser1.setUsername(username1);
-        testUser1.setName(name);
-        testUser1.setProfilePictureUrl(profilPictureUrl);
-        testUser1.setHomeLat(homeLat);
-        testUser1.setHomeLon(homeLon);
-
-        testUser1.setWeightFreshSnow(weightFreshSnow);
-        testUser1.setWeightSlopeLength(weightSlopeLength);
-        testUser1.setWeightTravelTime(weightTravelTime);
         testUser1.setWeightOccupancy(weightOccupancy1);
-
         userRepository.save(testUser1);
 
-        SkiResort skiResort1 = new SkiResort();
+        SkiResort skiResort1 = createSkiResort();
 
         String resortName = "ThirdRatingResort";
-        String region = "Tirol";
-        String operator = "Fake Gmbh";
-        String address = "Talstraße1";
-        Integer zip = 12345;
-        String city = "Fakecity";
-        Integer heightMin = 0;
-        Integer heightMax = 1000;
-        Double totalLength = 10.00;
-        Integer ropeways = 10;
-        String dateSeasonStart = "asdf";
-        String dateSeasonEnd  = "asdf";
-        String timeServiceStart  = "asdf";
-        String timeServiceEnd  = "asdf";
-        Integer currentUtilizationPercent = 1;
-        Integer snowDepthMin  = 1;
-        Integer snowDepthMax  = 1;
-        Integer amountFreshSnow = 1;
-        String dateLastSnowfall = "asdf";
-        String URLTicketpage = "asdf";
-        Integer avalancheWarningLevel = 1;
-        String URLImageFront = "asdf";
-        String URLImageSlope = "asdf";
-
         skiResort1.setName(resortName);
-        skiResort1.setRegion(region);
-        skiResort1.setOperator(operator);
-        skiResort1.setAddress(address);
-        skiResort1.setZip(zip);
-        skiResort1.setCity(city);
-        skiResort1.setHeightMin(heightMin);
-        skiResort1.setHeightMax(heightMax);
-        skiResort1.setTotalLength(totalLength);
-        skiResort1.setRopeWays(ropeways);
-        skiResort1.setDateSeasonStart(dateSeasonStart);
-        skiResort1.setDateSeasonEnd(dateSeasonEnd);
-        skiResort1.setTimeServiceStart(timeServiceStart);
-        skiResort1.setTimeServiceEnd(timeServiceEnd);
-        skiResort1.setCurrentUtilizationPercent(currentUtilizationPercent);
-        skiResort1.setSnowDepthMin(snowDepthMin);
-        skiResort1.setSnowDepthMax(snowDepthMax);
-        skiResort1.setAmountFreshSnow(amountFreshSnow);
-        skiResort1.setDateLastSnowfall(dateLastSnowfall);
-        skiResort1.setUrlTicketPage(URLTicketpage);
-        skiResort1.setAvalancheWarningLevel(avalancheWarningLevel);
-        skiResort1.setUrlImageFront(URLImageFront);
-        skiResort1.setUrlImageSlope(URLImageSlope);
-
         skiResortRepository.save(skiResort1);
-
         ratingService.calculateAllRating();
         /*
         if (ratingService.get(testUser, skiResort1).isPresent()) {
@@ -543,4 +217,79 @@ public class RatingServiceIntegrationTest {
         */
     }
 
+    public User createUser(){
+        User testUser = new User();
+
+        String name = "Horst";
+        String profilPictureUrl = "asdf";
+        Double homeLat = 46.00;
+        Double homeLon = 46.00;
+        Integer weightFreshSnow = 2;
+        Integer weightSlopeLength = 3;
+        Integer weightTravelTime = 4;
+        Integer weightOccupancy = 5;
+
+        testUser.setName(name);
+        testUser.setProfilePictureUrl(profilPictureUrl);
+        testUser.setHomeLat(homeLat);
+        testUser.setHomeLon(homeLon);
+        testUser.setWeightFreshSnow(weightFreshSnow);
+        testUser.setWeightSlopeLength(weightSlopeLength);
+        testUser.setWeightTravelTime(weightTravelTime);
+        testUser.setWeightOccupancy(weightOccupancy);
+
+        return testUser;
+    }
+
+    public SkiResort createSkiResort(){
+        SkiResort skiResort = new SkiResort();
+
+        String region = "Tirol";
+        String operator = "Fake Gmbh";
+        String address = "Talstraße1";
+        Integer zip = 12345;
+        String city = "FakeCity";
+        Integer heightMin = 0;
+        Integer heightMax = 1000;
+        Double totalLength = 10.00;
+        Integer ropeWays = 10;
+        String dateSeasonStart = "01-12-20xx";
+        String dateSeasonEnd = "01-05-20xx";
+        String timeServiceStart = "01-12-20xx";
+        String timeServiceEnd = "01-12-20xx";
+        Integer currentUtilizationPercent = 1;
+        Integer snowDepthMin = 1;
+        Integer snowDepthMax = 1;
+        Integer amountFreshSnow = 1;
+        String dateLastSnowfall = "01-12-20xx";
+        String URLTicketpage = "01-12-20xx";
+        Integer avalancheWarningLevel = 1;
+        String URLImageFront = "01-12-20xx";
+        String URLImageSlope = "01-12-20xx";
+
+        skiResort.setRegion(region);
+        skiResort.setOperator(operator);
+        skiResort.setAddress(address);
+        skiResort.setZip(zip);
+        skiResort.setCity(city);
+        skiResort.setHeightMin(heightMin);
+        skiResort.setHeightMax(heightMax);
+        skiResort.setTotalLength(totalLength);
+        skiResort.setRopeWays(ropeWays);
+        skiResort.setDateSeasonStart(dateSeasonStart);
+        skiResort.setDateSeasonEnd(dateSeasonEnd);
+        skiResort.setTimeServiceStart(timeServiceStart);
+        skiResort.setTimeServiceEnd(timeServiceEnd);
+        skiResort.setCurrentUtilizationPercent(currentUtilizationPercent);
+        skiResort.setSnowDepthMin(snowDepthMin);
+        skiResort.setSnowDepthMax(snowDepthMax);
+        skiResort.setAmountFreshSnow(amountFreshSnow);
+        skiResort.setDateLastSnowfall(dateLastSnowfall);
+        skiResort.setUrlTicketPage(URLTicketpage);
+        skiResort.setAvalancheWarningLevel(avalancheWarningLevel);
+        skiResort.setUrlImageFront(URLImageFront);
+        skiResort.setUrlImageSlope(URLImageSlope);
+
+        return skiResort;
+    }
 }
